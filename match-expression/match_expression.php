6e443ddef3d8c5cb52ee79dd89f5a3a70ff6cef0
beyond88 <muhin.cse.diu@gmail.com>
2021-11-14 00:53:06 +0600

    added match expression

--- match-expression/match_expression.php
+++ match-expression/match_expression.php
@@ -6,3 +6,32 @@
         500 => 'server error',
         default => 'unknown status code',
     };
+
+
+    $statusCode = 400;
+    $message = match ($statusCode) {
+        200 => 'perfect',
+    };
+
+    // UnhandledMatchError
+
+    #Combining conditions
+    $message = match ($statusCode) {
+        200, 300, 301, 302 => 'combined expressions',
+    };
+
+    //Complex conditions and performance
+    $message = [
+        $this->matchesRegex($line) => 'match A',
+        $this->matchesOtherRegex($line) => 'match B',
+    ][$line] ?? 'no match';
+
+    //Throwing exceptions
+    $message = match ($statusCode) {
+        200 => null,
+        500 => throw new ServerError(),
+        default => 'unknown status code',
+    };
+    
+    
+    
