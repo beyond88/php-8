d51d62e4d65f3659048d43c00c37b53b1d6b3f0e
beyond88 <muhin.cse.diu@gmail.com>
2021-11-13 17:33:57 +0600

    added named arguments

---
+++ named-arguments/name_arguments.php
@@ -0,0 +1,18 @@
+<?php
+
+class CustomerData
+{
+    public function __construct(
+        public string $name,
+        public string $email,
+        public int $age,
+    ) {}
+}
+
+$data = new CustomerData(
+    name: $input['name'],
+    email: $input['email'],
+    age: $input['age'],
+);
+
+$data = new CustomerData(...$customerRequest->validated());
